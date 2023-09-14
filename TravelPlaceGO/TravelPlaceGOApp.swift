ebf562da26b534b9825a5bea4382ef6927c14c28
Yunus Emre Bayezit <104377308+YEBay1@users.noreply.github.com>
2023-09-14 05:36:59 +0300

    Add files via upload

---
+++ TravelPlaceGO/TravelPlaceGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlaceGOApp.swift
+//  TravelPlaceGO
+//
+//  Created by Yunus Emre Bayezit on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlaceGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
